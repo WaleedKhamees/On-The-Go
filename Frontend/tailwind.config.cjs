5aeb719b68808885150f6c719bf7f4a40320a5c5
Walid-kh <walid.osama.khamees@gmail.com>
2022-11-10 20:23:19 +0200

    init project's frontend

---
+++ Frontend/tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      RedPrimary: "#C4141C",
+      RedSecondary: "#FFB4B7",
+      GreenPrimary: "#066B29",
+      GreenSecondary: "#9DFFBF",
+      White: "#FEFEFE",
+      Headings: "#000000",
+      Body: "#333333",
+      Small: "#989898",
+      Link: "#0067C5",
+    },
+    fontFamily: {
+      Inter: ["Inter", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
