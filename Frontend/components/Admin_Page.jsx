1eb6d2804f9041d5176404fe1af282d0492e677c
MohamedMaher02 <mohamed.02maher@gmail.com>
2022-12-02 15:33:09 +0200

    Add reservation Page

--- Frontend/components/Admin_Page.jsx
+++ Frontend/components/Admin_Page.jsx
@@ -20,17 +20,19 @@ function Admin_Page(props) {
                     <div className="flex gap-8 p-4">
                         <div className="flex-col space-y-2 ">
                             <h3 className="h3 text-[#989898]">Eastern</h3>
-                            <div className="flex gap-2 w-[276px] h-[34px] justify-between font-semibold ">
+                            <div className="flex items-center gap-2 w-[276px] h-[34px] justify-between font-semibold ">
                                 <h1 className="h1">{props.Eastern_Statistics}EGP</h1>
+
                                 <div className="flex rounded-lg w-[82px] h-[25px] justify-between px-1.5 " style={{ backgroundColor: (props.Eastern_percentage) > 0 ? "#9DFFBF" : "#FFB4B7" }}>
                                     <p>--</p>
                                     <p style={{ color: props.Eastern_percentage > 0 ? "#066B29" : "#C4141C" }}>{props.Eastern_percentage}%</p>
                                 </div>
+
                             </div>
                         </div>
                         <div className="flex-col space-y-2">
                             <h3 className="h3 text-[#989898]">Western</h3>
-                            <div className="flex gap-2 w-[276px] h-[34px] justify-between font-semibold">
+                            <div className="flex items-center gap-2 w-[276px] h-[34px] justify-between font-semibold">
                                 <h1 className="h1">{props.Western_Statistics}EGP</h1>
                                 <div className="flex rounded-lg w-[82px] h-[25px] justify-between px-1.5 " style={{ backgroundColor: (props.Western_percentage) > 0 ? "#9DFFBF" : "#FFB4B7" }}>
                                     <p>--</p>
@@ -40,7 +42,7 @@ function Admin_Page(props) {
                         </div>
                         <div className="flex-col space-y-2">
                             <h3 className="h3 text-[#989898]">Drinks</h3>
-                            <div className="flex gap-2 w-[276px] h-[34px] justify-between font-semibold">
+                            <div className="flex items-center gap-2 w-[276px] h-[34px] justify-between font-semibold">
                                 <h1 className="h1">{props.Drinks_Statistics}EGP</h1>
                                 <div className="flex rounded-lg w-[82px] h-[25px] justify-between px-1.5 " style={{ backgroundColor: (props.Drinks_percentage) > 0 ? "#9DFFBF" : "#FFB4B7" }}>
                                     <p>--</p>
