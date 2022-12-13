6d504bc705120a8ffeda19dab37d080758ac49eb
Walid-kh <walid.osama.khamees@gmail.com>
2022-12-13 17:35:25 +0200

    Feat: Adds Signup with Kinds

--- Backend/src/controllers/signupController.ts
+++ Backend/src/controllers/signupController.ts
@@ -9,7 +9,8 @@ export const signupController = {
         try {
             await client.query(`
             insert into userx (email, password, kind) values ('${user.email}', '${user.password}', '${user.kind ? user.kind : 'c'}');`);
-            await client.query(`
+            if (!user.kind || user.kind == 'c')
+                await client.query(`
             insert into customer (customer_id,first_name, last_name, wallet) values ((select userx_id from userx as u where u.email = '${user.email}'),'${user.first_name}', '${user.last_name}', ${user.wallet});`);
             res.status(201).json({ message: "success" });
         }
