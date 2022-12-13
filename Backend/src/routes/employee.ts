17f7c45bb03fde0a90eeb5d9504f0d9fa6811b94
MohamedMaher02 <mohamed.02maher@gmail.com>
2022-12-13 18:07:40 +0200

    Add employee backend (update and delete not work)

---
+++ Backend/src/routes/employee.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { employeeController } from "../controllers/employeeController"
+const router = Router();
+router.get("/employee", employeeController.getEmployees);
+
+router.get("/employee/:id", employeeController.getEmployee);
+
+router.patch("/employee/update", employeeController.updateEmployee);
+
+router.delete("/employee/delete/:id", employeeController.deleteEmployee);
+
+router.post("/employee/insert", employeeController.insertEmployee);
+
+
+module.exports = router; 
