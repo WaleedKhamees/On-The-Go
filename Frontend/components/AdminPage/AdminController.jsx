bfc7584065d011f3b1d82133176308d15840a989
Walid-kh <walid.osama.khamees@gmail.com>
2022-12-20 23:35:10 +0200

    admin page's crud backbone

--- Frontend/components/AdminPage/AdminController.jsx
+++ Frontend/components/AdminPage/AdminController.jsx
@@ -1,39 +1,64 @@
 import axios from "axios";
-import React, { useState } from "react";
-import { BiChevronDown } from "react-icons/bi";
-import { MdManageAccounts } from "react-icons/md";
+import React, { useEffect, useState } from "react";
+
+const EMPLOYEE_EDITABLE = {
+  employee_id: false,
+  first_name: true,
+  last_name: true,
+  typeofemployee: true,
+  supervise_id: true,
+  branch_id: true,
+  starttime: true,
+  endtime: true,
+  salary: true,
+};
+
 const AdminController = () => {
   // const [funcActive, setFuncActive] = useState(false);
   // const [funcHasChose, setFuncHasChose] = useState(false);
   // const [func, setfunc] = useState("What do you want to do?");
   const initFuncOptions = ["Access Employees", "Access Menu"];
-  const options = ["View", "Edit", "Delete", "Update"];
 
   const [initFunc, setInitFunc] = useState("");
-  const [employeeFunc, setEmployeeFunc] = useState("");
+  const selectInitFunc = async (func) => {
+    switch (func) {
+      case "Access Employees":
+        getEmployees();
+        break;
+      case "Access Menu":
+        break;
+    }
+    setInitFunc(func);
+  };
+
+
+
+
   const [employees, setEmployees] = useState([]);
+  const [employeesBeingEdited, setEmployeesBeingEdited] = useState({});
+
+  const [employeeInEditMode, setEmployeeInEditMode] = useState({
+    status: false,
+    rowKey: -1
+  });
+
+  useEffect(() => {
+
+    console.log(employeeInEditMode, employeesBeingEdited);
+
+  }, [employeesBeingEdited, employeeInEditMode])
+
   const getEmployees = async () => {
     const response = await axios.get("http://localhost:3000/employee");
     const employees = await response.data;
     setEmployees(employees);
     console.log(employees);
+  };
+
+  const handleEmployeeEdit = (employeeIndex, employee) => {
+    setEmployeeInEditMode({ rowKey: employeeIndex, status: true });
+    setEmployeesBeingEdited(employee);
   }
-
-  const handleEmployeeFuncChange = async (func) => {
-    switch (func) {
-      case "View":
-        getEmployees();
-        break;
-    }
-    setEmployeeFunc(func);
-  }
-
-
-
-  const [menuFunc, setMenuFunc] = useState("");
-
-
-
 
 
 
@@ -42,7 +67,7 @@ const AdminController = () => {
       <h3 className="h3">What do you want to do</h3>
 
       <select
-        onChange={(e) => { setInitFunc(e.target.value) }}
+        onChange={(e) => { selectInitFunc(e.target.value) }}
         defaultValue={"Select an option"}
         className="flex max-w-[350px] justify-between items-center cursor-pointer px-4 py-2 border border-Body w-full rounded-lg appearance-none select">
         <option value="Select an option" disabled>Select an option</option>
@@ -53,33 +78,77 @@ const AdminController = () => {
 
 
       {
-        initFunc === "Access Employees" &&
-        <>
-          <select
-            onChange={(e) => { handleEmployeeFuncChange(e.target.value) }}
-            defaultValue={"Select an option"}
-            className="flex justify-between max-w-[350px] items-center cursor-pointer px-4 py-2 border border-Body w-full rounded-lg appearance-none select">
-            <option value="Select an option" disabled>Select an option</option>
-            {options.map(func => <option value={func}>{func}</option>)}
-          </select>
-          {
-            employeeFunc === "View" && employees.length &&
-            <div className="w-4/5 mt-4 border rounded-lg overflow-clip">
-              <table className="w-full text-left">
-                <thead className="border-b ">
-                  <tr>
-                    {Object.keys(employees[0]).map(key => <th className="px-4 py-2" key={key}>{key.toUpperCase()}</th>)}
-                  </tr>
-                </thead>
-                <tbody>
-                  {employees.map(employee => <tr className="even:bg-[#1f2937] even:text-White odd:bg-[#374151] odd:text-White">{Object.values(employee).map(value => <td className="px-4 py-2">{value ?? "NONE"}</td>)}</tr>)}
-                </tbody>
-              </table>
-            </div>
+        initFunc === "Access Employees" && employees.length &&
+        <div className=" mt-4 border rounded-lg overflow-clip">
+          <table className="w-full text-left">
+            <thead className="border-b ">
+              <tr>
+                {Object.keys(employees[0]).map(key =>
+                  <th className="px-4 py-2" key={key}>{key.toUpperCase()}</th>
+                )}
+                <th className="px-4 py-2">Modify</th>
+              </tr>
+            </thead>
+            <tbody>
 
-          }
-        </>
+              {employees.map((employee, employeeIndex) =>
+                <tr
+                  key={employeeIndex}
+                  className={`${employeeIndex !== employeeInEditMode.rowKey ? "even:bg-[#4b4b4b] even:text-White odd:bg-[#d9d9d9] odd:text-body" : ""}`}>
+                  <>
+                    {/* Data  */}
+                    {
+                      Object.values(employee).map((value, i) =>
+                        <td
+                          key={value}
+                          className="px-4 py-2 focus:outline-none"
+                          onChange={(e) => {
+                            setEmployeesBeingEdited({ ...employeesBeingEdited })
+                          }}
+                          suppressContentEditableWarning
+                          contentEditable={employeeInEditMode.rowKey === employeeIndex && EMPLOYEE_EDITABLE[Object.keys(employee)[i]]}>
+                          {value ?? "NONE"}
+                        </td>
+                      )}
+                    {/* edit delete  */}
+                    {
+                      employeeInEditMode.rowKey !== employeeIndex
+                      &&
+                      <td className="px-4 py-2 cursor-pointer flex gap-1 justify-center">
+                        <span className="material-symbols-outlined" onClick={() => {
+                          handleEmployeeEdit(employeeIndex, employee);
+                        }}>
+                          edit
+                        </span>
+                        <span className="material-symbols-outlined">
+                          delete
+                        </span>
+                      </td>
+                    }
+                    {/* done */}
+                    {
+                      employeeInEditMode.rowKey === employeeIndex
+                      &&
+                      <td className="px-4 py-2 cursor-pointer flex gap-1 justify-center" onFocus={() => handleEdit()}>
+                        <span className="material-symbols-outlined">
+                          done
+                        </span>
+                      </td>
+                    }
+                  </>
+
+                </tr>)}
+
+            </tbody>
+          </table>
+        </div>
       }
+
+
+
+
+
+
     </>
   );
 };
@@ -94,15 +163,16 @@ Object.values(employee).map(attr => <td key={attr}>{attr}</td>))
       >
         <div className="flex items-center gap-4 w-full ">
           <MdManageAccounts fill="#333333" size={16} />
-          <p className={`p ${!funcHasChose ? "text-Small" : "text-Body"}`}>
+          <p className={p ${!funcHasChose ? "text-Small" : "text-Body"}`}>
             {func}
           </p>
         </div>
         <BiChevronDown size={16} />
       </div>
       <ul
-        className={`overflow-y-auto max-h-[120px] absolute w-full cursor-pointer z-50 transition-all ease-linear duration-1000 ${funcActive ? "block" : "hidden"
-          }`}
+        className={`overflow-y-auto max-h-[120px] absolute w - full cursor - pointer z - 50 transition - all ease - linear duration - 1000 ${
+                    funcActive? "block": "hidden"
+                  }`}
         onMouseLeave={(e) => setFuncActive(false)}
       >
         {funcOptions.map((option) => (
