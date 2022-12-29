32b0b80837f901c98352c27df4e9c9e37d9873f5
Walid-kh <walid.osama.khamees@gmail.com>
2022-12-29 03:37:20 +0200

    DONE

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { HashRouter } from "react-router-dom";
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+  <HashRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </HashRouter>
 );
